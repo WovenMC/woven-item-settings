35554418553ac849998ef8adf82a27e77fb06257
repulica <repulica@protonmail.com>
2020-09-16 02:22:41 -0700

    license headers, import cleanup

--- src/main/java/net/wovenmc/woven/api/item/settings/WovenItemSettings.java
+++ src/main/java/net/wovenmc/woven/api/item/settings/WovenItemSettings.java
@@ -1,7 +1,21 @@
+/*
+ * Copyright (c) 2020 WovenMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.wovenmc.woven.api.item.settings;
 
-import java.util.IdentityHashMap;
-import java.util.function.BiFunction;
 import java.util.function.Function;
 
 import org.jetbrains.annotations.Nullable;
@@ -10,7 +24,6 @@ import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
 
 public class WovenItemSettings extends Item.Settings {
